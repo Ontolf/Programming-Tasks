db7e6807e4c06fd5822983963b26f61bb30965c8
Ontolf <89953755+Ontolf@users.noreply.github.com>
2021-12-10 16:51:59 +0200

    Update Lab18_main.c

--- LabWorks/2lw_roots/Lab18_main.c
+++ LabWorks/2lw_roots/Lab18_main.c
@@ -3,7 +3,7 @@
 
 int main(){
     
-    float a = 0, b = 0, c =0, x = 0, fa = 0, fb = 0, fx = 0;
+    float a = 0, b = 0, c =0, x = 0, fa = 0, fb = 0, fx = 0, br = 0;
     int r = 1;
     
     printf("Enter value of left border\n" );
@@ -12,6 +12,8 @@ int main(){
     scanf("%f", &b);
     printf("Enter value of c wher f(x) = c\n" );
     scanf("%f", &c);
+    printf("Enter value of min borders distance \n" );
+    scanf("%f", &br);
     
     fa = atan(a); 
     fb = atan(b);
@@ -25,7 +27,7 @@ int main(){
     x = (a + b)/2;
     printf("%d%10.5f%10.5f%10.5f%10.5f%10.5f\n", r, a, b, x , atan(x), atan(c));
     
-    while( ( (b - a) > 0.1 ) || ( (a - b) > 0.1 ) ){
+    while( ( (b - a) > br ) || ( (a - b) > br ) ){
         r++;
          // printf("%10.5f%10.5f\n", atan(a),atan(b));
         if(atan(x) < atan(c) ){
