d5f030aa98d11240ae4f79f4d9dea3757960fb15
Ontolf <89953755+Ontolf@users.noreply.github.com>
2021-12-21 14:48:32 +0200

    Update Lab19_main.c

--- LabWorks/3lw_derivative/Lab19_main.c
+++ LabWorks/3lw_derivative/Lab19_main.c
@@ -3,7 +3,7 @@
 
 void main()
 {
-    float a = 0. , b = 5, x, step = 0.1, f11 = 0, f12 = 0, f21 = 0, f22 = 0, f112 = 0;
+    float a = 0. , b = 5, x, step = 0.1, f11 = 0, f12 = 0, f21 = 0, f22 = 0, f112 = 0, f111= 0;
     
     printf("Choose value of a:\n");
     scanf("%f", &a);
@@ -16,13 +16,14 @@ void main()
     x = a;
     while(x < b){
         
-        f11 = 1/(pow(x + step,2) + 1);
+        f11 = 1/(pow(x,2) + 1);
         f12 =  (atan(x + step) - atan(x))/step;
         
-        f21 = (-2 * (x + step))/( pow(x + step,4) + pow(x + step,2) + 1 );
+        f21 = (-2 * (x))/( pow(x,4) +2 * pow(x,2) + 1 );
         
+        f111 = 1/(pow(x + step,2) + 1);
         f112 = 1/(pow(x,2) + 1);
-        f22 = (f11 - f112)/step;
+        f22 = (f111 - f112)/step;
         
         printf(" %10.2f\t%10.2f\t%10.2f\t%10.2f\t%10.2f\t%10.2f\n", x, atan(x), f11, f12, f21, f22); 
 
