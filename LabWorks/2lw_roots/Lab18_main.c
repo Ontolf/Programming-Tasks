58e89f2bf1ad1faae3c224ecabd0e489edb771a9
Ontolf <89953755+Ontolf@users.noreply.github.com>
2022-01-06 11:59:11 +0200

    Update Lab18_main.c

--- LabWorks/2lw_roots/Lab18_main.c
+++ LabWorks/2lw_roots/Lab18_main.c
@@ -6,11 +6,11 @@ int main(){
     float a = 0, b = 0, c =0, x = 0, fa = 0, fb = 0, fx = 0, prec = 0;
     int r = 1;
     
-    printf("Enter value of left border\n" );
+    printf("Enter value of left border: \n" );
     scanf("%f", &a);
-    printf("Enter value of right border\n" );
+    printf("Enter value of right border: \n" );
     scanf("%f", &b);
-    printf("Enter value of c wher f(x) = c\n" );
+    printf("Enter value of c wher f(x) = c: \n" );
     scanf("%f", &c);
     printf("Enter value of precision: \n" );
     scanf("%f", &prec);
@@ -18,14 +18,14 @@ int main(){
     fa = atan(a); 
     fb = atan(b);
     
-    printf(" interval [%.2f;%.2f] where atan(x)\n", a, b);
+    printf(" Interval [%.2f;%.2f] where atan(x)\n", a, b);
         
-    printf("from a, atan(%7.3f) = %7.3f\n", a, fa);
-    printf("from b, atan(%7.3f) = %7.3f\n", b, fb);
+    printf("From a, atan(%7.3f) = %7.3f\n", a, fa);
+    printf("From b, atan(%7.3f) = %7.3f\n", b, fb);
     
-    printf("\ta\tb\tx\tatan(x)\n");
+    printf("inter\ta\tb\tx\tatan(x)\n");
     x = (a + b)/2;
-    printf("%10.5f%10.5f%10.5f%10.5f \n", a, b, x , atan(x));
+    printf("%2d%10.5f%10.5f%10.5f%10.5f \n", r, a, b, x , atan(x));
     
     while( ( (b - a) > prec ) || ( (a - b) > prec ) ){
         r++;
@@ -38,8 +38,7 @@ int main(){
         fb = atan(x);
         }
         x = (a + b)/2;
-        printf("%10.5f%10.5f%10.5f%10.5f\n", a, b, c , atan(x));
+        printf("%2d%10.5f%10.5f%10.5f%10.5f\n", r, a, b, c , atan(x));
     }
     
-    printf("Number of interactions is: %d ", r);
 }
